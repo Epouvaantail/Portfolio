bd6454b300e3d7852432ad755c2b27156f39aa2a
Epouvaantail <coutouzis.kilian@gmail.com>
2023-09-18 16:17:38 +0200

    update

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -40,11 +40,11 @@ const projects = fetch('./assets/projects.json',{ method:'get'})
       function generate(data) {
         for (let d in data){
           projects.insertAdjacentHTML('beforeend', `
-            <div class="project tile" id="${data[d].id}">
+            <div class="project reveal" id="${data[d].id}">
               <img src="${data[d].imageUrl}" alt="${data[d].title}">
                 <div class="description">
                   <p>${data[d].description}</p>
-			            <div>
+			            <div class="projectFooter">
 			              <button onClick="window.location.href = '${data[d].code}'" class="github-code slide">
 			                <a id='a' href="${data[d].code}">
 		  	                <img src="./assets/images/logo/github-mark.png" alt="logo-github"/>
@@ -63,22 +63,23 @@ const projects = fetch('./assets/projects.json',{ method:'get'})
         }
       }
     };
-    generate(data)
+  generate(data)
 
-    let elementsArray = document.querySelectorAll('.tile');
-    window.addEventListener('scroll', fadeIn ); 
-    function fadeIn() {
-      for (var i = 0; i < elementsArray.length; i++) {
-        var elem = elementsArray[i]
-        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
-        if (distInView < 0) {
-            elem.classList.add("inView");
-        } else {
-            elem.classList.remove("inView");
-        }
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 50;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
       }
     }
-  fadeIn();
+  }
+  window.addEventListener("scroll", reveal);
+  reveal();
 })
 .catch(error => {
   console.error(error)
