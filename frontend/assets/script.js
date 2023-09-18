993ba7ad1614803fbcf339ebfb4b80b8dc31149e
Epouvaantail <coutouzis.kilian@gmail.com>
2023-09-18 15:25:07 +0200

    test

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -27,62 +27,62 @@ setInterval(clock, 1000);
 
 /* ---------------------------------------------------------------------------------- */
 
-// const projects = fetch('https://portfolio-whve.onrender.com/api/project',{ method:'get'})
-// .then(projects => {
-//   if(!projects.ok) {
-//     throw new Error('Erreur de chargement des projects');
-//   }
-//   return projects.json();
-// })
-// .then(data => {
-// 	const projects = document.querySelector('.projects');
-//     for (let d in data) {
-//       function generate(data) {
-//         for (let d in data){
-//           projects.insertAdjacentHTML('beforeend', `
-//             <div class="project tile" id="${data[d].id}">
-//               <img src="${data[d].imageUrl}" alt="${data[d].title}">
-//                 <div class="description">
-//                   <p>${data[d].description}</p>
-// 			            <div>
-// 			              <button onClick="window.location.href = '${data[d].code}'" class="github-code slide">
-// 			                <a id='a' href="${data[d].code}">
-// 		  	                <img src="./assets/images/logo/github-mark.png" alt="logo-github"/>
-// 		                    Voir le code
-// 		                  </a>
-// 				            </button>
-// 				            <button onClick="window.location.href = '${data[d].site}/'" class="site slide">
-// 				              <a id='a' href="${data[d].site}/">Voir le site</a>
-// 			              </button>
-// 		              </div>
-// 				        </div>
-//                 <ul class="langTools"> ${data[d].languages} </ul>
-//             </div>
-//             <hr>`
-//           )
-//         }
-//       }
-//     };
-//     generate(data)
+const projects = fetch('./assets/projects.json',{ method:'get'})
+.then(projects => {
+  if(!projects.ok) {
+    throw new Error('Erreur de chargement des projects');
+  }
+  return projects.json();
+})
+.then(data => {
+	const projects = document.querySelector('.projects');
+    for (let d in data) {
+      function generate(data) {
+        for (let d in data){
+          projects.insertAdjacentHTML('beforeend', `
+            <div class="project tile" id="${data[d].id}">
+              <img src="${data[d].imageUrl}" alt="${data[d].title}">
+                <div class="description">
+                  <p>${data[d].description}</p>
+			            <div>
+			              <button onClick="window.location.href = '${data[d].code}'" class="github-code slide">
+			                <a id='a' href="${data[d].code}">
+		  	                <img src="./assets/images/logo/github-mark.png" alt="logo-github"/>
+		                    Voir le code
+		                  </a>
+				            </button>
+				            <button onClick="window.location.href = '${data[d].site}/'" class="site slide">
+				              <a id='a' href="${data[d].site}/">Voir le site</a>
+			              </button>
+		              </div>
+				        </div>
+                <ul class="langTools"> ${data[d].languages} </ul>
+            </div>
+            <hr>`
+          )
+        }
+      }
+    };
+    generate(data)
 
-//     let elementsArray = document.querySelectorAll('.tile');
-//     window.addEventListener('scroll', fadeIn ); 
-//     function fadeIn() {
-//       for (var i = 0; i < elementsArray.length; i++) {
-//         var elem = elementsArray[i]
-//         var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
-//         if (distInView < 0) {
-//             elem.classList.add("inView");
-//         } else {
-//             elem.classList.remove("inView");
-//         }
-//       }
-//     }
-//   fadeIn();
-// })
-// .catch(error => {
-//   console.error(error)
-// })
+    let elementsArray = document.querySelectorAll('.tile');
+    window.addEventListener('scroll', fadeIn ); 
+    function fadeIn() {
+      for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+      }
+    }
+  fadeIn();
+})
+.catch(error => {
+  console.error(error)
+})
 
 
 /* ---------------------------------------------------------------------------------- */
